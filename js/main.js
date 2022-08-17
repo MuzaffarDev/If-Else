b118b3ac0cb6291fa103868cbb6005ee6c4982e2
MuzaffaR <abdurahmonovmuzaffar56@gmail.com>
2022-08-17 18:41:36 +0500

    ozgartirildi

--- js/main.js
+++ js/main.js
@@ -16,7 +16,19 @@ let xarajtlarYigindisi = (bilet + mehmonxona) * dollar + aylanish * yevro;
 let usermoney = prompt("Joriy mablag'ingizni kiriting!", 2000000);
 
 if (xarajtlarYigindisi >= usermoney) {
-  console.log(`Afsus ${firstname} sizda ${travel}ga borgani mablag' yetarli emas`);
+  console.log(
+    `Afsus ${firstname} sizda ${travel}ga borgani mablag' yetarli emas`
+  );
+  alert(
+    `Afsus ${firstname} sizda ${travel}ga borgani${
+      xarajtlarYigindisi - usermoney
+    } mablag' yetarli emas`
+  );
 } else {
-  console.log(`${firstname} sizda ${travel}ga borishga mablag' yetarli yoningizda yana ozgina pul olvoling `);
+  console.log(
+    `${firstname} sizda ${travel}ga borishga mablag' yetarli yoningizda yana ozgina pul olvoling `
+  );
+  alert(
+    `${firstname} sizda ${travel}ga borishga  mablag' yetarli yoningizda yana ozgina pul olvoling `
+  );
 }
